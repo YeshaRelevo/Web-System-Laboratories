6fb6001062011551718c74321b4addd94e1a4439
YeshaRelevo <princesstumbaga2@gmail.com>
2024-10-22 15:21:16 +0800

    activity3

---
+++ Activity 2/hehe.js
@@ -0,0 +1,34 @@
+// const getData = () =>{
+//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+//  .then(response =>{
+//     console.log(response)
+//     return response;
+//  }).then(result =>{
+//     const data = result.jason()
+//     return data
+//  }).then(data =>{
+//     console.log(data)
+//  }).catch(err =>{
+//     console.log(err)
+//  })
+// }
+
+const getData = async() =>{
+  const response =
+     await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  const data = await response.json()
+
+  return data
+}
+
+
+console.log(1)
+console.log(2)
+getData().then(data =>{
+    console.log(data)
+}).catch(err =>{
+    console.log(err)
+})
+console.log(3)
+console.log(4)
+console.log(5)
